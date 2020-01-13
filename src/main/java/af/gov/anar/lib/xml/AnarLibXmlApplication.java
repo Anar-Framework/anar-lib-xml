53945aec85688b4e7db69257921f1aba0c79553d
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-13 14:43:19 +0430

    xml utility library for anar framework

---
+++ src/main/java/af/gov/anar/lib/xml/AnarLibXmlApplication.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.xml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnarLibXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnarLibXmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/af/gov/anar/lib/xml/AnarLibXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarLibXmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
